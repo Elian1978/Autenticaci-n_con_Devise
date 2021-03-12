8100320dae7f8363a9b49104ccfc2b473b5a9a35
Elian Rojas <impresosgosen@gmail.com>
2021-03-12 14:22:46 -0600

    LISTO SQLLITE A POSTGRESS

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get 'index/home'
+  devise_for :users
+
+  root to: "index#home"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
